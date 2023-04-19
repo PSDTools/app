01fdd58928fd981dcfcba83e839894e13ecdf70d
Parker <hasenkap@student.psdr3.org>
2023-04-19 12:34:45 -0500

    changed color and name for funsies

--- lib/main.dart
+++ lib/main.dart
@@ -22,9 +22,9 @@ class MyApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.green,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Maybe A Wallet app???'),
     );
   }
 }
