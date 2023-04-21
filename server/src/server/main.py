2b169da287121e711dc1331bba1d43bf345b4e32
Eli <88557639+lishaduck@users.noreply.github.com>
2023-04-21 18:17:37 -0500

    docs: add strings [skip ci]

--- server/src/server/main.py
+++ server/src/server/main.py
@@ -1,9 +1,15 @@
+"""This is the server
+
+It might be made with [FastAPI](https://fastapi.tiangolo.com/) and possibly [Strawberry](https://strawberry.rocks/docs/integrations/fastapi).
+"""
+
+
 from aiohttp import web
 from rich import traceback
 
 
-
 async def handle(request):
+    """Handle requests with [aiohttp](https://docs.aiohttp.org/en/stable/)."""
     name = request.match_info.get("name", "Anonymous")
     text = "Hello, " + name
     return web.Response(text=text)
@@ -14,6 +20,7 @@ app.add_routes([web.get("/", handle), web.get("/{name}", handle)])
 
 
 def main():
+    """The main function of the program."""
     web.run_app(app)
 
 
