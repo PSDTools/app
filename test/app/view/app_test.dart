28d72907642e293c0da2787d818f0103ac9cb8f8
Eli <88557639+lishaduck@users.noreply.github.com>
2023-05-15 15:52:35 -0500

    ci: fix tests

--- test/app/view/app_test.dart
+++ test/app/view/app_test.dart
@@ -1,12 +1,12 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:pirate_code_3/app/app.dart';
-import 'package:pirate_code_3/counter/counter.dart';
 
 void main() {
   group('App', () {
-    testWidgets('renders CounterPage', (tester) async {
+    testWidgets('renders a Material App', (tester) async {
       await tester.pumpWidget(App());
-      expect(find.byType(CounterPage), findsOneWidget);
+      expect(find.byType(MaterialApp), findsOneWidget);
     });
   });
 }
