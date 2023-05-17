b04517ce2ca4bd15a8cbf584f6cd94edeb1c5fe4
Eli <88557639+lishaduck@users.noreply.github.com>
2023-05-17 14:00:00 -0500

    chore: remove bloc
    I wan't using it, and wasn't intending on it.

--- lib/bootstrap.dart
+++ lib/bootstrap.dart
@@ -1,26 +1,9 @@
 import "dart:async";
 import "dart:developer";
 
-import "package:bloc/bloc.dart";
 import "package:flutter/widgets.dart";
 import "package:flutter_riverpod/flutter_riverpod.dart";
 import "package:flutter_web_plugins/flutter_web_plugins.dart";
-
-class AppBlocObserver extends BlocObserver {
-  const AppBlocObserver();
-
-  @override
-  void onChange(BlocBase<dynamic> bloc, Change<dynamic> change) {
-    super.onChange(bloc, change);
-    log("onChange(${bloc.runtimeType}, $change)");
-  }
-
-  @override
-  void onError(BlocBase<dynamic> bloc, Object error, StackTrace stackTrace) {
-    log("onError(${bloc.runtimeType}, $error, $stackTrace)");
-    super.onError(bloc, error, stackTrace);
-  }
-}
 
 Future<void> bootstrap(FutureOr<ConsumerWidget> Function() builder) async {
   FlutterError.onError = (details) {
@@ -28,8 +11,6 @@ Future<void> bootstrap(FutureOr<ConsumerWidget> Function() builder) async {
   };
 
   usePathUrlStrategy();
-
-  Bloc.observer = const AppBlocObserver();
 
   await runZonedGuarded(
     () async => runApp(
