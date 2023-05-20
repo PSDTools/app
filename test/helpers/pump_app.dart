3ae2e049d9ef84adae369154ba447a8bb4a1c1fb
Eli <88557639+lishaduck@users.noreply.github.com>
2023-05-20 11:14:37 -0500

    fix: ignore `avoid-top-level-members-in-tests` `PumpApp` isn't a test.

--- test/helpers/pump_app.dart
+++ test/helpers/pump_app.dart
@@ -2,6 +2,8 @@ import "package:flutter/material.dart";
 import "package:flutter_test/flutter_test.dart";
 import "package:pirate_code_3/l10n/l10n.dart";
 
+// This isn't a test.
+// ignore: avoid-top-level-members-in-tests
 extension PumpApp on WidgetTester {
   Future<void> pumpApp(Widget widget) {
     return pumpWidget(
