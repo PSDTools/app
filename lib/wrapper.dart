30dce70d6d9c8e0e663447759d1e9957c84e56c5
Eli <88557639+lishaduck@users.noreply.github.com>
2023-05-22 07:56:29 -0500

    chore: make `Wrapper` use more private widgets

--- lib/wrapper.dart
+++ lib/wrapper.dart
@@ -12,7 +12,7 @@ import "app/app_router.dart";
 class WrapperPage extends ConsumerWidget {
   const WrapperPage({super.key});
 
-  static const tabs = [
+  static const mobileTabs = [
     Tab(text: "1", icon: Icon(Icons.abc)),
     Tab(text: "2", icon: Icon(Icons.abc)),
     Tab(text: "3", icon: Icon(Icons.abc)),
@@ -41,60 +41,8 @@ class WrapperPage extends ConsumerWidget {
             }
 
             final page = constraints.maxWidth < 450
-                ? Scaffold(
-                    appBar: AppBar(
-                      leading: const AutoLeadingButton(),
-                      title: Text(context.topRoute.name),
-                      bottom: const TabBar(tabs: tabs),
-                    ),
-                    body: _MainArea(child: child),
-                    bottomNavigationBar: BottomNavigationBar(
-                      unselectedIconTheme: const IconThemeData(
-                        color: Color.fromARGB(255, 0, 0, 0),
-                      ),
-                      selectedIconTheme: const IconThemeData(
-                        color: Color.fromARGB(255, 39, 131, 0),
-                      ),
-                      items: const [
-                        BottomNavigationBarItem(
-                          icon: Icon(Icons.home),
-                          label: "Home",
-                        ),
-                        BottomNavigationBarItem(
-                          icon: Icon(Icons.favorite),
-                          label: "Favorites",
-                        ),
-                      ],
-                      onTap: tabsRouter.setActiveIndex,
-                      currentIndex: tabsRouter.activeIndex,
-                    ),
-                  )
-                : Scaffold(
-                    body: Row(
-                      children: [
-                        NavigationRail(
-                          extended: constraints.maxWidth >= 600,
-                          destinations: const [
-                            NavigationRailDestination(
-                              icon: Icon(Icons.home),
-                              label: Text("Home"),
-                            ),
-                            NavigationRailDestination(
-                              icon: Icon(Icons.favorite),
-                              label: Text("Favorites"),
-                            ),
-                          ],
-                          selectedIndex: tabsRouter.activeIndex,
-                          onDestinationSelected: tabsRouter.setActiveIndex,
-                        ),
-                        Expanded(
-                          child: _MainArea(
-                            child: child,
-                          ),
-                        ),
-                      ],
-                    ),
-                  );
+                ? _MobileWrapper(mobileTabs: mobileTabs, child: child)
+                : _ExpandedWrapper(constraints: constraints, child: child);
 
             return WillPopScope(
               onWillPop: onWillPop,
@@ -102,7 +50,7 @@ class WrapperPage extends ConsumerWidget {
                 child: AnimatedSwitcher(
                   duration: const Duration(milliseconds: 200),
                   child: DefaultTabController(
-                    length: tabs.length,
+                    length: mobileTabs.length,
                     child: page,
                   ),
                 ),
@@ -111,6 +59,98 @@ class WrapperPage extends ConsumerWidget {
           },
         );
       },
+    );
+  }
+}
+
+class _ExpandedWrapper extends ConsumerWidget {
+  const _ExpandedWrapper({
+    required this.child,
+    required this.constraints,
+    // Temporary ignore, see <dart-lang/sdk#49025>.
+    // ignore: unused_element
+    super.key,
+  });
+
+  final Widget child;
+  final BoxConstraints constraints;
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final tabsRouter = AutoTabsRouter.of(context);
+
+    return Scaffold(
+      body: Row(
+        children: [
+          NavigationRail(
+            extended: constraints.maxWidth >= 600,
+            destinations: const [
+              NavigationRailDestination(
+                icon: Icon(Icons.home),
+                label: Text("Home"),
+              ),
+              NavigationRailDestination(
+                icon: Icon(Icons.favorite),
+                label: Text("Favorites"),
+              ),
+            ],
+            selectedIndex: tabsRouter.activeIndex,
+            onDestinationSelected: tabsRouter.setActiveIndex,
+          ),
+          Expanded(
+            child: _MainArea(
+              child: child,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _MobileWrapper extends ConsumerWidget {
+  const _MobileWrapper({
+    required this.child,
+    required this.mobileTabs,
+    // Temporary ignore, see <dart-lang/sdk#49025>.
+    // ignore: unused_element
+    super.key,
+  });
+
+  final Widget child;
+  final List<Tab> mobileTabs;
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final tabsRouter = AutoTabsRouter.of(context);
+
+    return Scaffold(
+      appBar: AppBar(
+        leading: const AutoLeadingButton(),
+        title: Text(context.topRoute.name),
+        bottom: TabBar(tabs: mobileTabs),
+      ),
+      body: _MainArea(child: child),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: "Home",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.favorite),
+            label: "Favorites",
+          ),
+        ],
+        onTap: tabsRouter.setActiveIndex,
+        currentIndex: tabsRouter.activeIndex,
+        selectedIconTheme: const IconThemeData(
+          color: Color.fromARGB(255, 39, 131, 0),
+        ),
+        unselectedIconTheme: const IconThemeData(
+          color: Color.fromARGB(255, 0, 0, 0),
+        ),
+      ),
     );
   }
 }
