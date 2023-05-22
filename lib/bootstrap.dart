245e97977e79fc2dc825554d544f020a0cfd66e6
Eli <88557639+lishaduck@users.noreply.github.com>
2023-05-22 09:51:26 -0500

    Revert "fix: maybe?"
    This reverts commit a4d937d8e9b04a641c3149eadc5d5403dfc45c6c.

--- lib/bootstrap.dart
+++ lib/bootstrap.dart
@@ -3,17 +3,14 @@ import "dart:developer";
 
 import "package:flutter/widgets.dart";
 import "package:flutter_riverpod/flutter_riverpod.dart";
-import "package:flutter_web_plugins/url_strategy.dart";
+import "package:flutter_web_plugins/flutter_web_plugins.dart";
 
 Future<void> bootstrap(FutureOr<ConsumerWidget> Function() builder) async {
   FlutterError.onError = (details) {
     log(details.exceptionAsString(), stackTrace: details.stack);
   };
 
-  // if (flutter)
-  {
-    usePathUrlStrategy();
-  }
+  usePathUrlStrategy();
 
   await runZonedGuarded(
     () async => runApp(
