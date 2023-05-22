3c8ef6fc1d4c90b6687bae905da74ab6c72f41aa
Eli <88557639+lishaduck@users.noreply.github.com>
2023-05-22 07:38:31 -0500

    feat: make bootstrapping it's own mixin

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,31 +1,10 @@
-import "dart:async";
-import "dart:developer";
-
-import "package:flutter/material.dart";
 import "package:flutter_riverpod/flutter_riverpod.dart";
-import "package:flutter_web_plugins/flutter_web_plugins.dart";
 
+import "bootstrap.dart";
 import "view/app.dart";
 
 export "app_router.dart";
 
-class App extends ConsumerWidget with AppView {
+class App extends ConsumerWidget with AppView, Bootstrap {
   App({super.key});
-
-  Future<void> bootstrap() async {
-    FlutterError.onError = (details) {
-      log(details.exceptionAsString(), stackTrace: details.stack);
-    };
-
-    usePathUrlStrategy();
-
-    await runZonedGuarded(
-      () async => runApp(
-        ProviderScope(
-          child: this,
-        ),
-      ),
-      (error, stackTrace) => log(error.toString(), stackTrace: stackTrace),
-    );
-  }
 }
