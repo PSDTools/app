b09790d9a3b3c06e08cebc698dfb6487f7e31205
Parker <hasenkap@student.psdr3.org>
2023-05-24 11:02:08 -0500

    feat:dual screen

--- lib/widgets/jimmy/jimmy_page.dart
+++ lib/widgets/jimmy/jimmy_page.dart
@@ -11,109 +11,135 @@ class JimmyPage extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return Center(
-      child: GridView(
-        primary: false,
-        padding: const EdgeInsets.all(20),
-        gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
-          maxCrossAxisExtent: 400,
-          mainAxisSpacing: 20,
-          crossAxisSpacing: 20,
-        ),
-        children: [
-          Container(
-            padding: const EdgeInsets.all(16),
-            decoration: const BoxDecoration(
-              color: Color.fromARGB(255, 253, 255, 178),
-              borderRadius: BorderRadius.all(Radius.circular(20)),
-              boxShadow: [
-                BoxShadow(
-                  color: Color.fromARGB(59, 0, 0, 0),
-                  blurRadius: 10,
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: GridView(
+              primary: false,
+              padding: const EdgeInsets.all(20),
+              gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+                maxCrossAxisExtent: 400,
+                mainAxisSpacing: 20,
+                crossAxisSpacing: 20,
+                childAspectRatio: 1,
+              ),
+              children: [
+                Container(
+                  padding: const EdgeInsets.all(16),
+                  decoration: const BoxDecoration(
+                    color: Color.fromARGB(255, 253, 255, 178),
+                    borderRadius: BorderRadius.all(Radius.circular(20)),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Color.fromARGB(59, 0, 0, 0),
+                        blurRadius: 10,
+                      ),
+                    ],
+                  ),
+                  child: Column(
+                    children: <Widget>[
+                      const FittedBox(
+                        fit: BoxFit.cover,
+                        child: Text(
+                          "Pirate Coins",
+                          style: TextStyle(fontSize: 80),
+                        ),
+                      ),
+                      FittedBox(
+                        fit: BoxFit.fitHeight,
+                        child: Image.asset(Assets.images.treasure.path),
+                      ),
+                    ],
+                  ),
                 ),
-              ],
-            ),
-            child: Column(
-              children: <Widget>[
-                const FittedBox(
-                  fit: BoxFit.cover,
-                  child: Text("Pirate Coins", style: TextStyle(fontSize: 80)),
+                Container(
+                  padding: const EdgeInsets.all(8),
+                  decoration: const BoxDecoration(
+                    color: Color.fromARGB(255, 242, 184, 184),
+                    borderRadius: BorderRadius.all(Radius.circular(20)),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Color.fromARGB(59, 0, 0, 0),
+                        blurRadius: 10,
+                      ),
+                    ],
+                  ),
+                  child: const Text(" "),
                 ),
-                FittedBox(
-                  fit: BoxFit.fitHeight,
-                  child: Image.asset(Assets.images.treasure.path),
+                Container(
+                  padding: const EdgeInsets.all(8),
+                  decoration: const BoxDecoration(
+                    color: Color.fromARGB(255, 178, 254, 186),
+                    borderRadius: BorderRadius.all(Radius.circular(20)),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Color.fromARGB(59, 0, 0, 0),
+                        blurRadius: 10,
+                      ),
+                    ],
+                  ),
+                  child: const Text(" "),
+                ),
+                Container(
+                  padding: const EdgeInsets.all(8),
+                  decoration: const BoxDecoration(
+                    color: Color.fromARGB(255, 249, 183, 255),
+                    borderRadius: BorderRadius.all(Radius.circular(20)),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Color.fromARGB(59, 0, 0, 0),
+                        blurRadius: 10,
+                      ),
+                    ],
+                  ),
+                  child: const Text(" "),
+                ),
+                Container(
+                  padding: const EdgeInsets.all(8),
+                  decoration: const BoxDecoration(
+                    color: Color.fromARGB(255, 187, 198, 255),
+                    borderRadius: BorderRadius.all(Radius.circular(20)),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Color.fromARGB(59, 0, 0, 0),
+                        blurRadius: 10,
+                      ),
+                    ],
+                  ),
+                  child: const Text(" "),
+                ),
+                Container(
+                  padding: const EdgeInsets.all(8),
+                  decoration: const BoxDecoration(
+                    color: Color.fromARGB(255, 255, 205, 130),
+                    borderRadius: BorderRadius.all(Radius.circular(20)),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Color.fromARGB(59, 0, 0, 0),
+                        blurRadius: 10,
+                      ),
+                    ],
+                  ),
+                  child: const Text(" "),
                 ),
               ],
             ),
           ),
-          Container(
-            padding: const EdgeInsets.all(8),
-            decoration: const BoxDecoration(
-              color: Color.fromARGB(255, 242, 184, 184),
-              borderRadius: BorderRadius.all(Radius.circular(20)),
-              boxShadow: [
-                BoxShadow(
-                  color: Color.fromARGB(59, 0, 0, 0),
-                  blurRadius: 10,
-                ),
-              ],
+          Expanded(
+            child: Container(
+              padding: const EdgeInsets.all(8),
+              decoration: const BoxDecoration(
+                color: Color.fromARGB(255, 255, 205, 130),
+                borderRadius: BorderRadius.all(Radius.circular(20)),
+                boxShadow: [
+                  BoxShadow(
+                    color: Color.fromARGB(59, 0, 0, 0),
+                    blurRadius: 10,
+                  ),
+                ],
+              ),
+              child: const Text(" "),
             ),
-            child: const Text(" "),
-          ),
-          Container(
-            padding: const EdgeInsets.all(8),
-            decoration: const BoxDecoration(
-              color: Color.fromARGB(255, 178, 254, 186),
-              borderRadius: BorderRadius.all(Radius.circular(20)),
-              boxShadow: [
-                BoxShadow(
-                  color: Color.fromARGB(59, 0, 0, 0),
-                  blurRadius: 10,
-                ),
-              ],
-            ),
-            child: const Text(" "),
-          ),
-          Container(
-            padding: const EdgeInsets.all(8),
-            decoration: const BoxDecoration(
-              color: Color.fromARGB(255, 249, 183, 255),
-              borderRadius: BorderRadius.all(Radius.circular(20)),
-              boxShadow: [
-                BoxShadow(
-                  color: Color.fromARGB(59, 0, 0, 0),
-                  blurRadius: 10,
-                ),
-              ],
-            ),
-            child: const Text(" "),
-          ),
-          Container(
-            padding: const EdgeInsets.all(8),
-            decoration: const BoxDecoration(
-              color: Color.fromARGB(255, 187, 198, 255),
-              borderRadius: BorderRadius.all(Radius.circular(20)),
-              boxShadow: [
-                BoxShadow(
-                  color: Color.fromARGB(59, 0, 0, 0),
-                  blurRadius: 10,
-                ),
-              ],
-            ),
-            child: const Text(" "),
-          ),
-          Container(
-            padding: const EdgeInsets.all(8),
-            decoration: const BoxDecoration(
-              color: Color.fromARGB(255, 255, 205, 130),
-              borderRadius: BorderRadius.all(Radius.circular(20)),
-              boxShadow: [
-                BoxShadow(
-                  color: Color.fromARGB(59, 0, 0, 0),
-                  blurRadius: 10,
-                ),
-              ],
-            ),
-            child: const Text(" "),
           ),
         ],
       ),
