1ec48b83aab0c1d7b85bc063d900cf120dcf6ffe
Eli Dukes <88557639+lishaduck@users.noreply.github.com>
2023-06-08 20:27:50 -0500

    chore: write some tests

---
+++ test/app/bootstrap_test.dart
@@ -0,0 +1,29 @@
+import "package:flutter/material.dart";
+import "package:flutter/src/widgets/framework.dart";
+import "package:flutter_riverpod/flutter_riverpod.dart";
+import "package:flutter_test/flutter_test.dart";
+import "package:pirate_code/app/bootstrap.dart";
+
+import "../helpers/helpers.dart";
+
+class _BootstrapMixed extends ConsumerWidget with Bootstrap {
+  const _BootstrapMixed({
+    // Temporary ignore, see <dart-lang/sdk#49025>.
+    // ignore: unused_element
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return const Text("");
+  }
+}
+
+void main() {
+  group("Bootstrappin' Tests", () {
+    testWidgets("Test the boots", (tester) async {
+      await tester.pumpApp(const _BootstrapMixed());
+      expect(find.text(""), findsOneWidget);
+    });
+  });
+}
