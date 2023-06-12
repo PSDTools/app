4b21f7511dfbe0b5bf082b3bb5db3ead7b92314a
Eli Dukes <88557639+lishaduck@users.noreply.github.com>
2023-06-12 12:54:13 -0500

    feat: fix up the server

--- server/src/server/main.py
+++ server/src/server/main.py
@@ -6,36 +6,24 @@ It might be made with [FastAPI](https://fastapi.tiangolo.com/) and possibly [Str
 
 from __future__ import annotations
 
-from typing import TYPE_CHECKING
-
-import strawberry
+import uvicorn
 from fastapi import FastAPI
 from rich import traceback
-from strawberry.fastapi import GraphQLRouter
 
-if TYPE_CHECKING:
-    from typing_extensions import Self
+app: FastAPI = FastAPI()
 
 
-@strawberry.type
-class Query:
-    """A query."""
-
-    def hello(self: Self) -> str:
-        """Hello."""
-        return "Hello World"
+@app.get("/")
+def read_root() -> dict[str, str]:
+    """Say hello to the world."""
+    return {"Hello": "World"}
 
 
 def main() -> None:
     """Run the program."""
-    schema = strawberry.Schema(Query)
-    graphql_app = GraphQLRouter(schema)
-
-    app: FastAPI = FastAPI()
-    app.include_router(graphql_app, prefix="/graphql")
+    uvicorn.run(app, host="localhost", port=8000)
 
 
 if __name__ == "__main__":
     traceback.install()
-    main()
     main()
