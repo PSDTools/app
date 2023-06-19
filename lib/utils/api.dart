7897c457822135d3b6e3183faae4658b21940060
Eli Dukes <88557639+lishaduck@users.noreply.github.com>
2023-06-19 16:07:14 -0500

    feat: secrets management

--- lib/utils/api.dart
+++ lib/utils/api.dart
@@ -1,5 +1,7 @@
+import "../dart_define.gen.dart";
+
 class ApiInfo {
   ApiInfo._();
   static const String url = "https://cloud.appwrite.io/v1";
-  static const String projectId = "648b3836d14e06cddc4e";
+  static const String projectId = DartDefine.projectId;
 }
