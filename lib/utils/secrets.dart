2bc72c29221d9301d584c1d21a888f2c8e92ae77
Eli Dukes <88557639+lishaduck@users.noreply.github.com>
2023-06-21 16:30:14 -0500

    fix: more type mismatches

--- lib/utils/secrets.dart
+++ lib/utils/secrets.dart
@@ -5,11 +5,15 @@ import "../dart_define.gen.dart";
 part "secrets.g.dart";
 
 @riverpod
-Flavor flavor() {
+// Required for riverpod.
+// ignore: avoid-unused-parameters
+Flavor flavor(FlavorRef ref) {
   return DartDefine.flavor;
 }
 
 @riverpod
-String projectId() {
+// Required for riverpod.
+// ignore: avoid-unused-parameters
+String projectId(ProjectIdRef ref) {
   return DartDefine.projectId;
 }
