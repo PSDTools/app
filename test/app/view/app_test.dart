c7852e361e1d91794823d8c1a63888bfcc267729
Eli Dukes <88557639+lishaduck@users.noreply.github.com>
2023-06-21 16:57:32 -0500

    fix: test
    Hey, at least the last commit fixed a problem-to-be.

--- test/app/view/app_test.dart
+++ test/app/view/app_test.dart
@@ -1,11 +1,12 @@
 import "package:flutter/material.dart";
+import "package:flutter_riverpod/flutter_riverpod.dart";
 import "package:flutter_test/flutter_test.dart";
 import "package:pirate_code/app/app.dart";
 
 void main() {
   group("App", () {
     testWidgets("renders a Material App", (tester) async {
-      await tester.pumpWidget(App());
+      await tester.pumpWidget(ProviderScope(child: App()));
       expect(find.byType(MaterialApp), findsOneWidget);
     });
   });
