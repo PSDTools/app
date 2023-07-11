68a883e8dea9df06d4915cd9a3fa5d474aabfb1b
Eli <88557639+lishaduck@users.noreply.github.com>
2023-07-11 16:52:12 -0500

    fix: caps
    Signed-off-by: Eli <88557639+lishaduck@users.noreply.github.com>

--- lib/utils/data/secrets.dart
+++ lib/utils/data/secrets.dart
@@ -9,24 +9,24 @@ part "secrets.g.dart";
 
 /// Get the flavor of the application.
 @riverpod
-Flavor flavor(FlavorRef _) => Dartdefine.flavor;
+Flavor flavor(FlavorRef _) => DartDefine.flavor;
 
 /// Get the Appwrite project ID.
 @riverpod
-String projectId(ProjectIdRef _) => Dartdefine.projectId;
+String projectId(ProjectIdRef _) => DartDefine.projectId;
 
 /// Get the Appwrite Api Endpoint
 @riverpod
-String apiUrl(ApiUrlRef ref) => Dartdefine.apiEndpoint;
+String apiUrl(ApiUrlRef ref) => DartDefine.apiEndpoint;
 
 /// Get the Appwrite database ID.
 @riverpod
-String databaseId(DatabaseIdRef _) => Dartdefine.databaseId;
+String databaseId(DatabaseIdRef _) => DartDefine.databaseId;
 
 /// Get the Appwrite collection ID.
 @riverpod
-String collectionId(CollectionIdRef _) => Dartdefine.collectionId;
+String collectionId(CollectionIdRef _) => DartDefine.collectionId;
 
 /// Get the Appwrite document ID.
 @riverpod
-String documentId(DocumentIdRef _) => Dartdefine.documentId;
+String documentId(DocumentIdRef _) => DartDefine.documentId;
