ac7367f07d1d2d40f420d33c08b5bf047d8d257d
Eli <88557639+lishaduck@users.noreply.github.com>
2023-07-24 15:23:50 -0500

    refactor: use dart 3 switches
    Fixes: #123.

--- lib/features/utils/presentation/device_info_dialog.dart
+++ lib/features/utils/presentation/device_info_dialog.dart
@@ -65,61 +65,92 @@ class _GetContent extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final flavorConfig = ref.watch(flavorConfigProvider);
-    final currentBuildMode = ref.watch(buildModeProvider);
     final deviceInfo = ref.watch(deviceInfoProvider);
 
-    return deviceInfo.when(
-      data: (device) {
-        final stringUtils = ref.watch(stringUtilsProvider);
-        final buildMode = stringUtils.enumName(currentBuildMode.toString());
-
-        final androidView = [
-          _BuildTile("Manufacturer:", device.manufacturer),
-          _BuildTile("Android version:", device.release),
-          _BuildTile("Android SDK:", "${device.sdkInt}"),
-        ];
-
-        final iosView = [
-          _BuildTile("Device:", device.name),
-          _BuildTile("System name:", device.systemName),
-          _BuildTile("System version:", device.systemVersion),
-        ];
-
-        final platformView = switch (device.device) {
-          Device.android => androidView,
-          Device.ios => iosView,
-          Device.other => const [
-              _BuildTile(
-                "Oops.",
-                "Chances are, you're neither on Android nor iOS.",
-              ),
+    switch (deviceInfo) {
+      case AsyncData(:final value):
+        return _View(value: value);
+      case AsyncLoading():
+        return const Center(
+          child: Column(
+            children: [
+              CircularProgressIndicator(),
+              Text("Loading..."),
             ],
-        };
-
-        return ListView(
-          children: [
-            _BuildTile("Flavor:", flavorConfig.name),
-            _BuildTile("Build mode:", buildMode),
-            _BuildTile("Physical device?:", "${device.isPhysicalDevice}"),
-            _BuildTile("Model:", device.model),
-            ...platformView,
-          ],
+          ),
         );
-      },
-      error: (err, stack) {
-        final message = "An issue occurred: $err, $stack.";
+      case AsyncError(:final error, :final stackTrace):
+        final message = "An issue occurred: $error, $stackTrace.";
 
         return Text(message);
-      },
-      loading: () => const Center(
-        child: Column(
-          children: [
-            CircularProgressIndicator(),
-            Text("Loading..."),
-          ],
-        ),
-      ),
+      default:
+        return const Text("Unknown");
+    }
+  }
+}
+
+class _View extends ConsumerWidget {
+  const _View({
+    required this.value,
+    // Temporary ignore, see <dart-lang/sdk#49025>.
+    // ignore: unused_element
+    super.key,
+  });
+
+  final DeviceData value;
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final flavorConfig = ref.watch(flavorConfigProvider);
+    final currentBuildMode = ref.watch(buildModeProvider);
+    final stringUtils = ref.watch(stringUtilsProvider);
+    final buildMode = stringUtils.enumName(currentBuildMode.toString());
+
+    final platformView = switch (value.device) {
+      Device.android => androidView(value),
+      Device.ios => iosView(value),
+      Device.other => otherView(value),
+    };
+
+    return ListView(
+      children: [
+        _BuildTile("Flavor:", flavorConfig.name),
+        _BuildTile("Build mode:", buildMode),
+        _BuildTile("Physical device?:", "${value.isPhysicalDevice}"),
+        _BuildTile("Model:", value.model),
+        ...platformView,
+      ],
     );
+  }
+
+  List<_BuildTile> iosView(DeviceData value) {
+    final iosView = [
+      _BuildTile("Device:", value.name),
+      _BuildTile("System name:", value.systemName),
+      _BuildTile("System version:", value.systemVersion),
+    ];
+
+    return iosView;
+  }
+
+  List<_BuildTile> androidView(DeviceData value) {
+    final androidView = [
+      _BuildTile("Manufacturer:", value.manufacturer),
+      _BuildTile("Android version:", value.release),
+      _BuildTile("Android SDK:", "${value.sdkInt}"),
+    ];
+
+    return androidView;
+  }
+
+  List<_BuildTile> otherView(DeviceData value) {
+    const otherView = [
+      _BuildTile(
+        "Oops.",
+        "Chances are, you're neither on Android nor iOS.",
+      ),
+    ];
+
+    return otherView;
   }
 }
