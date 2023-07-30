1eb7d5996c9ad7dc29c947d225eeb218dadd60f0
PetalCat <phasenkamp27@gmail.com>
2023-07-29 20:21:52 -0500

    feat: add coins

--- lib/features/pirate_coins/data/coins_data.dart
+++ lib/features/pirate_coins/data/coins_data.dart
@@ -8,16 +8,71 @@ import "../../../utils/api.dart";
 
 part "coins_data.g.dart";
 
+/// A repository for coin manipulation.
+abstract interface class CoinsRepository {
+  /// Get coins data.
+  Future<int> coinsData();
+
+  /// Add coins to the database.
+  Future<int> addCoins();
+
+  /// Remove coins from the database.
+  Future<int> removeCoins();
+}
+
+/// The default implementation of [CoinsRepository].
+class AppwriteCoinsRepository implements CoinsRepository {
+  /// Create a new instance of [AppwriteCoinsRepository].
+  AppwriteCoinsRepository(this.client);
+
+  /// The Appwrite client.
+  final Client client;
+
+  @override
+  Future<int> coinsData() async {
+    final database = Databases(client);
+    final data = await database.getDocument(
+      databaseId: apiInfo.databaseId,
+      collectionId: apiInfo.collectionId,
+      documentId: apiInfo.documentId,
+    );
+    return data.data["Coins"] as int;
+  }
+
+  ///Add coins to the database
+  @override
+  Future<int> addCoins() async {
+    final database = Databases(client);
+    final data = await database.updateDocument(
+      databaseId: apiInfo.databaseId,
+      collectionId: apiInfo.collectionId,
+      documentId: apiInfo.documentId,
+      data: {
+        "Coins": 1,
+      },
+    );
+    return data.data["Coins"] as int;
+  }
+
+  /// Remove coins from the database
+  Future<int> removeCoins() async {
+    final database = Databases(client);
+    final data = await database.updateDocument(
+      databaseId: apiInfo.databaseId,
+      collectionId: apiInfo.collectionId,
+      documentId: apiInfo.documentId,
+      data: {
+        "Coins": -1,
+      },
+    );
+    return data.data["Coins"] as int;
+  }
+}
+
 /// Get coins data.
 @riverpod
-Future<int> coinsData(CoinsDataRef ref) async {
+CoinsRepository coinsData(CoinsDataRef ref) {
   final client = ref.watch(clientProvider);
-  final database = Databases(client);
-  final data = await database.getDocument(
-    databaseId: apiInfo.databaseId,
-    collectionId: apiInfo.collectionId,
-    documentId: apiInfo.documentId,
-  );
 
-  return data.data["Coins"] as int;
+  return AppwriteCoinsRepository(client);
 }
