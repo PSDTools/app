49419e94adc1cda57cc8b88a30da2f81cc6beee7
Eli <88557639+lishaduck@users.noreply.github.com>
2023-09-30 21:08:34 -0500

    chore: remove some unnecessary json serialization

--- lib/features/utils/domain/banner_model.dart
+++ lib/features/utils/domain/banner_model.dart
@@ -5,26 +5,20 @@ import "package:flutter/foundation.dart";
 import "package:flutter/material.dart";
 import "package:freezed_annotation/freezed_annotation.dart";
 
-import "../../../utils/json_converters.dart";
 import "../../../utils/models.dart";
 
 part "banner_model.freezed.dart";
-part "banner_model.g.dart";
 
 /// Represent the development banner's configuration.
 @freezed
 @immutable
-sealed class BannerConfig with _$BannerConfig implements Model {
+sealed class BannerConfig with _$BannerConfig {
   /// Create a new, immutable instance of [BannerConfig].
   const factory BannerConfig({
     /// The name to display on the banner.
     required String bannerName,
 
     /// The color of the banner.
-    @ColorConverter() required Color bannerColor,
+    required Color bannerColor,
   }) = _BannerConfig;
-
-  /// Convert a JSON [Map] into a new, immutable instance of [BannerConfig].
-  factory BannerConfig.fromJson(Map<String, Object?> json) =>
-      _$BannerConfigFromJson(json);
 }
