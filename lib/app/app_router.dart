4c67d61b860e8f8dc2293692c72b4910b7a3c726
Eli <88557639+lishaduck@users.noreply.github.com>
2023-11-21 20:12:39 -0600

    fix: lint

--- lib/app/app_router.dart
+++ lib/app/app_router.dart
@@ -9,8 +9,8 @@ import "../features/auth/presentation/auth_page/auth_page.dart";
 import "../features/dashboard/presentation/dashboard_page/dashboard_page.dart";
 import "../features/dashboard/presentation/wrapper_page/wrapper_page.dart";
 import "../features/gpa_calculator/presentation/gpa_page/gpa_page.dart";
-import '../features/pirate_coins/presentation/pirate_coins_page/pirate_coins_student_page.dart';
-import '../features/pirate_coins/presentation/pirate_coins_page/pirate_coins_teacher_page.dart';
+import "../features/pirate_coins/presentation/pirate_coins_page/pirate_coins_student_page.dart";
+import "../features/pirate_coins/presentation/pirate_coins_page/pirate_coins_teacher_page.dart";
 import "../features/pirate_coins/presentation/stats_page/stats_page.dart";
 
 part "app_router.gr.dart";
